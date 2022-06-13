b9c3d9b820e4c6db77d0de4581b86dfff3157ebd
tadin-project <takumiinui24@gmail.com>
2022-06-14 03:19:21 +0700

    Persiapan api selesai

--- app/Controllers/API/Data.php
+++ app/Controllers/API/Data.php
@@ -3,7 +3,7 @@
 namespace App\Controllers\API;
 
 use App\Controllers\BaseController;
-use App\Models\DataModel;
+use App\Models\DataRecordModel;
 
 class Data extends BaseController
 {
@@ -11,31 +11,12 @@ class Data extends BaseController
 
     public function __construct()
     {
-        $this->M_data_device = new DataModel();
+        $this->M_data_device = new DataRecordModel();
     }
 
     public function store()
     {
         $data = [
-            'v1'        => addslashes($this->request->getVar('v1')),
-            'v2'        => addslashes($this->request->getVar('v2')),
-            'v3'        => addslashes($this->request->getVar('v3')),
-            'i1'        => addslashes($this->request->getVar('i1')),
-            'i2'        => addslashes($this->request->getVar('i2')),
-            'i3'        => addslashes($this->request->getVar('i3')),
-            'w1'        => addslashes($this->request->getVar('w1')),
-            'w2'        => addslashes($this->request->getVar('w2')),
-            'w3'        => addslashes($this->request->getVar('w3')),
-            'kwh'       => addslashes($this->request->getVar('kwh')),
-            'pf1'       => addslashes($this->request->getVar('pf1')),
-            'pf2'       => addslashes($this->request->getVar('pf2')),
-            'pf3'       => addslashes($this->request->getVar('pf3')),
-            'magnitude' => addslashes($this->request->getVar('magnitude')),
-            'timer'     => addslashes($this->request->getVar('timer')),
-            'waktu'     => $this->request->getVar('waktu'),
-            'output'    => addslashes($this->request->getVar('output')),
-            'kondisi'   => addslashes($this->request->getVar('kondisi')),
-            'gangguan'  => addslashes($this->request->getVar('gangguan')),
             'record_at' => addslashes($this->request->getVar('record_at')),
         ];
 
